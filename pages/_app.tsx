5779ef210fa84a55b144efad5efa1f0502d1b423
nto300002 <samonkntd@gmail.com>
2022-10-21 23:47:28 +0900

    add: Layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Header from './component/Header'
+import Layout from './component/Layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
